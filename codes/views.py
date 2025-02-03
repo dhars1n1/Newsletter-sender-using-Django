341844f2bcadee77e85984477f2ecf71ba287a23
Dharsini Sri B <161904561+dhars1n1@users.noreply.github.com>
2025-02-03 08:33:17 +0530

    Add files via upload

---
+++ codes/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+def send_mail_page(request):
+    
+    context = {}
+
+    if request.method == 'POST':
+        address = request.POST.get('address')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        if address and subject and message:
+            try:
+                send_mail(subject, message, settings.EMAIL_HOST_USER, [address])
+                context['result'] = 'Email sent successfully!'
+            except Exception as e:
+                context['result'] = f'Whoops! Error sending email!: {e}'
+        
+        else:
+            context['result'] = 'Please fill all the fields!'
+        
+        
+    return render(request, "index.html", context)
